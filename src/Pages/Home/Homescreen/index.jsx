e814129ac3ba80729bef140d4d2aab47b6c71df7
PinkCatCodes <brigita.ce@gmail.com>
2024-04-01 10:48:07 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/Pages/Home/Homescreen/index.jsx
@@ -0,0 +1,23 @@
+import HeroSection from "../HeroSection";
+import MySkills from "../MySkills";
+import AboutMe from "../AboutMe";
+import MyPortfolio from "../MyPortfolio";
+//import Testimonials from "../Testimonials";
+import ContactMe from "../ContactMe";
+import Footer from "../Footer";
+
+export default function Home() {
+  return (
+    //react fragments to avoid empty divs
+    //add Testimonials
+    <>
+      <HeroSection />
+      <MyPortfolio />
+
+      <AboutMe />
+      <MySkills />
+      <ContactMe />
+      <Footer />
+    </>
+  );
+}
